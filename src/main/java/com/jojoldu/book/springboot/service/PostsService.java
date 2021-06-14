7bb340698796ba3cc4db2713b128dcbee6ad958a
722020026 <yeslihan@naver.com>
2021-06-14 17:37:09 +0900

    update source

---
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot.service;
+
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+//    public Long save(Po)
+}
---
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,8 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostsApiController {
+
+}
